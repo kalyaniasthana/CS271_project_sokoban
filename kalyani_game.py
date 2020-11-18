8d277c45b9e36de98bdcea06b3a90741100d5f51
Kalyani Asthana <kalyaniasthana@gmail.com>
2020-11-18 14:33:14 -0800

    board class and initial parse

---
+++ kalyani_game.py
@@ -0,0 +1,55 @@
+import os # will mostly use this to NOT hard code paths
+
+#I think it's a good idea to use an OOP approach for this project
+class board:
+
+	#initialise board variables 
+
+	def __init__(self, board_input_file):
+		self.sizeH, self.sizeV, self.nWallSquares, self.wallCoordinates, self.nBoxes, \
+		self.boxCoordinates, self.nstorLocations, self.storCoordinates, self.playerLoc = \
+		None, None, None, None, None, None, None, None, None
+		self.board_input_file = board_input_file
+
+	#parse input file - sokobanXY.txt
+
+	def __str__(self):
+		return 'sizeH: {self.sizeH}, sizeV: {self.sizeV}, nWallSquares: {self.nWallSquares}, wallCoordinates: {self.wallCoordinates}, nBoxes: {self.nBoxes}, boxCooridates: {self.boxCoordinates}, nstorLocations: {self.nstorLocations}, storCoordinates: {self.storCoordinates}, playerLoc: {self.playerLoc}'.format(self = self)
+
+	def string_to_int_list(self, string_list):
+		return list(map(int, string_list))
+
+	def parse(self):
+
+		'''input is expected to have 5 lines
+		line 1 is the sizeH, sizeV
+		line 2 is nWallSquares followed by a list wallCoordinates
+		line 3 is a list of boxCordinates
+		line 4 is a list of storCoordinates (storage location coordinates)
+		line 5 is the initial position of SOKOBAN'''
+
+		line_number = 1 # to iterate through the lines
+		with open(self.board_input_file) as f:
+			for line in f:
+
+				l_line = line.split(' ')
+
+				if line_number == 1:
+					self.sizeH, self.sizeV = int(l_line[0]),int(l_line[1])
+				elif line_number == 2:
+					self.nWallSquares,self.wallCoordinates = int(l_line[0]),self.string_to_int_list(l_line[1: ])
+				elif line_number == 3:
+					self.nBoxes,self.boxCoordinates = int(l_line[0]),self.string_to_int_list(l_line[1: ])
+				elif line_number == 4:
+					self.nstorLocations,self.storCoordinates = int(l_line[0]),self.string_to_int_list(l_line[1: ])
+				elif line_number == 5:
+					self.playerLoc = (int(l_line[0]),int(l_line[1])) #(x,y)
+				line_number += 1
+
+def main():
+	board_input_file = os.path.join(os.getcwd(), 'input_files', 'sokoban00.txt')
+	sokoban_board = board(board_input_file)
+	sokoban_board.parse()
+	print(sokoban_board)
+
+main()
