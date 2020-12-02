de9e5fdbbf66e0041e775688a7bf07cdddc3e859
Kalyani Asthana <kalyaniasthana@gmail.com>
2020-12-01 23:04:40 -0800

    major bug fixes

--- sokoban_generator.py
+++ sokoban_generator.py
@@ -1,89 +1,73 @@
 import os
 
-class Sokoban_Generator:
+class SokobanInputConverter:
 
-    def __init__(self, board_input_file, number):
-        #initialise board variables
+    def __init__(self, boardInputFile, number):
+        # initialise board variables
         self.sizeH, self.sizeV, self.nWallSquares, self.wallCoordinates, self.nBoxes, \
         self.boxCoordinates, self.nstorLocations, self.storCoordinates, self.playerLoc = \
         0, 0, 0, [], 0, [], 0, [], ()
-        self.board_input_file = board_input_file
+        self.boardInputFile = boardInputFile
         self.number = number
 
-    def parse(self): #parse input file - inputXY.txt to sokobanXY.txt
+    def parse(self): # parse input file - inputXY.txt to sokobanXY.txt
 
         """Explanation ...."""
-        with open(self.board_input_file) as f:
-            i = 0
-            for line in f:
-                if i == 0:
-                    l_line = line.split(' ')
-                    self.sizeH = int(l_line[0])
-                    self.sizeV = int(l_line[1])
-                else:
-                    for j in range(self.sizeH):
-                        char = line[j]
-                        self.adder(char, j, i)
-                i += 1
-
+        assert os.path.isfile(self.boardInputFile)
+        with open(self.boardInputFile) as fin:
+            countLines = 0
+            for line in fin:
+                currentLine = list(line.strip('\n'))
+                if countLines == 0:
+                    self.sizeH = len(currentLine)
+                for c in range(self.sizeH):
+                    currentChar = currentLine[c]
+                    self.adder(currentChar, c, countLines)
+                countLines += 1
+        self.sizeV = countLines
         self.writing_on_file()
 
-    def adder(self, char, i, j): #converts a square in inputXY depending on its content
+    def adder(self, char, i, j): # converts a square in inputXY depending on its content
         if char == '@':
-            self.playerLoc = (i, j)
+            self.playerLoc = (j+1, i+1)
         elif char == '#':
             self.nWallSquares += 1
-            self.wallCoordinates.append((i,j))
+            self.wallCoordinates.append((j+1, i+1))
         elif char == '$':
             self.nBoxes += 1
-            self.boxCoordinates.append((i,j))
+            self.boxCoordinates.append((j+1, i+1))
         elif char == '.':
             self.nstorLocations += 1
-            self.storCoordinates.append((i,j))
-
-
-
+            self.storCoordinates.append((j+1, i+1))
 
     def writing_on_file(self):
-        path = os.path.join(os.getcwd(), 'input_files', "sokoban"+self.number+".txt")
-        with open(path, "a") as f2:
-            for i in range(4):
-                if i == 0:
-                    # Line 1 is the sizeH, sizeV
-                    f2.write(str(self.sizeH)+" "+str(self.sizeV))
-                    f2.write("\n") #add the breakline
-                elif i == 1:
-                    f2.write(str(self.nWallSquares))
-                    # Line 2 is the number of walls and the list of coordinates
-                    for k in self.wallCoordinates:
-                        f2.write(" "+str(k[0])+" "+str(k[1]))
-                    f2.write("\n") #add the breakline
-                elif i == 2:
-                    # Line 3 is the number of boxes and the list of coordinates
-                    f2.write(str(self.nBoxes))
-                    # Line 2 is the number of walls and the list of coordinates
-                    for k in self.boxCoordinates:
-                        f2.write(" "+str(k[0])+" "+str(k[1]))
-
-                    f2.write("\n") #add the breakline
-                elif i == 3:
-                    # Line 4 is the number of storage places and its coordinates
-                    f2.write(str(self.nstorLocations))
-                    # Line 2 is the number of walls and the list of coordinates
-                    for k in self.storCoordinates:
-                        f2.write(" "+str(k[0])+" "+str(k[1]))
-
-                    f2.write("\n") #add the breakline
-                elif i == 4:
-                    # Line 5 is the initial position of player
-                    f2.write(str(self.playerLoc[0])+" "+str(self.playerLoc[1]))
+        path = os.path.join(os.getcwd(), "input_files", "sokoban"+self.number+".txt")
+        with open(path, "w") as fout:
+            # Line 1 is the sizeH, sizeV
+            fout.write(str(self.sizeH)+" "+str(self.sizeV)+"\n")
+            # Line 2 is the number of walls and the list of wall coordinates
+            fout.write(str(self.nWallSquares))
+            for wallCoord in self.wallCoordinates:
+                fout.write(" "+str(wallCoord[0])+" "+str(wallCoord[1]))
+            fout.write("\n") # linebreak
+            # Line 3 is the number of boxes and the list of box coordinates
+            fout.write(str(self.nBoxes))
+            for boxCoord in self.boxCoordinates:
+                fout.write(" "+str(boxCoord[0])+" "+str(boxCoord[1]))
+            fout.write("\n") # linebreak
+            # Line 4 is the number of storage places and the list of storage coordinates
+            fout.write(str(self.nstorLocations))
+            for storCoord in self.storCoordinates:
+                fout.write(" "+str(storCoord[0])+" "+str(storCoord[1]))
+            fout.write("\n") # linebreak
+            # Line 5 is the initial position of player
+            fout.write(str(self.playerLoc[0])+" "+str(self.playerLoc[1]))
 
 def main():
-    print("Introduce number of inputXY.txt you want to generate as sokobanXY.txt (example 01)")
-    str_number = str(input())
-    board_input_file = os.path.join(os.getcwd(), 'input_files', 'input'+str_number+'.txt')
-    sokoban_board = Sokoban_Generator(board_input_file, str_number)
-    sokoban_board.parse()
-
+    print("Enter XY for the inputXY.txt you want to convert to sokobanXY.txt (example 01)")
+    strFileNumber = str(input())
+    boardInputFile = os.path.join(os.getcwd(), 'input_files', 'input'+strFileNumber+'.txt')
+    sokobanBoard = SokobanInputConverter(boardInputFile, strFileNumber)
+    sokobanBoard.parse()
 
 main()
